a2b76f52df989a7d7526fee1fed0dd49a58dc5c0
SydSerr <sydneroo1@gmail.com>
2025-02-20 18:24:52 -0800

    combined file semi cleaned

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import pandas as pd
-import torch.nn as nn
-import torch.optim as optim
-import torch
-from sklearn.preprocessing import StandardScaler
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import numpy as np
+# creating main file for projectgit
 
 df = pd.read_csv()
