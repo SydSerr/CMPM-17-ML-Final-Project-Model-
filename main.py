12854187d9a10f448109ebc40c6114356bd91019
SydSerr <sydneroo1@gmail.com>
2025-02-22 21:32:30 -0800

    Cleaned dataset

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import re
 
 df1 = pd.read_csv("descriptions.csv", on_bad_lines="skip") #skip glitched lines
 df1.info()
-print(df1["app_id"].unique()) #view the unique values for debugging the different languages
+#print(df1["app_id"].unique()) #view the unique values for debugging the different languages
 df1 = df1.dropna(ignore_index=True)
 df1 = df1.drop_duplicates (ignore_index=True)
 df1 = df1.drop(columns= "summary")
@@ -35,7 +35,7 @@ df1.info()
 
 
 df2 = pd.read_csv("genres.csv")
-print(df2["app_id"].unique())
+#print(df2["app_id"].unique())
 df2.info()
 df2 = df2.dropna(ignore_index=True)
 df2 = df2.drop_duplicates (ignore_index=True)
@@ -44,30 +44,29 @@ df2.info()
 
 df = pd.concat([df1,df2],axis=1)
 print(df)
+
 df.info()
 
-print(df)
 
 print(f'What null vals are there?\n{df.loc[df.isna().any(axis=1)]}')
 df = df.dropna(ignore_index=True) 
 print(f'What null vals are there?\n{df.loc[df.isna().any(axis=1)]}')
 
-
-print(f'Unique vals in genre col\n{df["genre"].unique()}')
-print(f'Unique vals in extensive col\n{df["extensive"].unique()}')
+print(f'Initial unique vals in genre col\n{df["genre"].unique()}')
+print(f'Initial unique vals in extensive col\n{df["extensive"].unique()}')
 
 df["genre"] = df["genre"].replace("Экшены","Action")
 df["genre"] = df["genre"].replace("Бесплатные","Free to Play")
 df["genre"] = df["genre"].replace("Стратегии","Strategy")
 df["genre"] = df["genre"].replace("Ação","Action")
 df["genre"] = df["genre"].replace("Приключенческие игры","Adventure")
-df["genre"] = df["genre"].replace("Ролевые игры","Roleplay")
-df["genre"] = df["genre"].replace("Rol","Roleplay")
+df["genre"] = df["genre"].replace("Ролевые игры","RPG")
+df["genre"] = df["genre"].replace("Rol","RPG")
 df["genre"] = df["genre"].replace("Akční","Action")
 df["genre"] = df["genre"].replace("Dobrodružné","Adventure")
 df["genre"] = df["genre"].replace("动作","Action")
 df["genre"] = df["genre"].replace("策略","Strategy")
-df["genre"] = df["genre"].replace("角色扮演","Roleplay")
+df["genre"] = df["genre"].replace("角色扮演","RPG")
 df["genre"] = df["genre"].replace("Acción","Action")
 df["genre"] = df["genre"].replace("Aventure","Adventure")
 df["genre"] = df["genre"].replace("Симуляторы","Simulation")
@@ -85,7 +84,7 @@ df["genre"] = df["genre"].replace("アクション","Action")
 df["genre"] = df["genre"].replace("アドベンチャー","Adventure")
 df["genre"] = df["genre"].replace("インディー","Indie")
 df["genre"] = df["genre"].replace("Simulationen","Simulation")
-df["genre"] = df["genre"].replace("Rollenspiel","Roleplay")
+df["genre"] = df["genre"].replace("Rollenspiel","RPG")
 df["genre"] = df["genre"].replace("冒险","Adventure")
 df["genre"] = df["genre"].replace("Eventyr","Adventure")
 df["genre"] = df["genre"].replace("Strategi","Strategy")
@@ -95,7 +94,7 @@ df["genre"] = df["genre"].replace("Azione","Action")
 df["genre"] = df["genre"].replace("Actie","Action")
 df["genre"] = df["genre"].replace("Пригоди","Adventure")
 df["genre"] = df["genre"].replace("Estrategia","Strategy")
-df["genre"] = df["genre"].replace("Roolipelit","Roleplay")
+df["genre"] = df["genre"].replace("Roolipelit","RPG")
 df["genre"] = df["genre"].replace("Seikkailu","Adventure")
 df["genre"] = df["genre"].replace("Strategia","Strategy")
 df["genre"] = df["genre"].replace("Ранний доступ","Early Access")
@@ -105,8 +104,19 @@ df["genre"] = df["genre"].replace("独立","Indie")
 df["genre"] = df["genre"].replace("Massively Multiplayer","Multiplayer")
 df["genre"] = df["genre"].replace("Free To Play","Free to Play")
 df["genre"] = df["genre"].replace("Abenteuer","Adventure")
+df["genre"] = df["genre"].replace("Indépendant","Indie")
 
-print(f'Unique vals in genre col\n{df["genre"].unique()}')
+
+print(f'Final unique vals in genre col\n{df["genre"].unique()}')
+
+#one hot encode the genre column
+df = pd.get_dummies(df,columns=["genre"])
+
+print(f'These are the duplicates:\n{df.loc[df.duplicated()]}') #empty no duplicates in df
+
+df = df.drop(columns="app_id")
+
+df.info()
 
 df.to_csv("cleaned_dataset.csv")
 
