8140e9af560b19c24465e022c3c07a83b4a1a7a5
SydSerr <sydneroo1@gmail.com>
2025-03-14 19:38:05 -0700

    fixed demo file with processing user input

--- demo.py
+++ demo.py
@@ -1,28 +1,34 @@
 from myRNN import myRNN
 import torch
+import torch.nn.functional 
 from main import every_letter
-import numpy as np
 
-model = myRNN(59,150,10)
-model.load_state_dict(torch.load("demo.pth", weights_only = True))
-
+# Load model
+model = myRNN(59, 150, 10)
+model.load_state_dict(torch.load("demo.pth"))  
 model.eval()
 
 genres = ["Action", "Adventure", "Casual", "Indie", "Multiplayer", "RPG", "Racing", "Simulation", "Sports", "Strategy"]
 
 def pred_genre(user_description):
     input_processed = every_letter(user_description)
-    input_tensor = torch.tensor(input_processed, dtype = torch.long).unsqueeze(0)
-    print(input_tensor.shape)
-    hidden = model.initHidden()
- 
-    output, _ = model(input_tensor, hidden)
-    pred_index = torch.argmax(output, dim=1).item()
+    # one-hot encode character indices 
+    input_tensor = torch.tensor(input_processed, dtype=torch.long)
+    input_tensor = torch.nn.functional .one_hot(input_tensor, num_classes=59).float().unsqueeze(0) 
 
-    return genres[pred_index]
+    # Initialize hidden state
+    hidden = model.initHidden()
+
+    # Pass input sequentially
+    for i in range(input_tensor.shape[1]):  # Iterates through each char
+        output, hidden = model(input_tensor[:, i], hidden)
+    
+    # Gets predicted index
+    pred_index = torch.argmax(output, dim=1).item()  
+
+    return genres[pred_index] 
 
 if __name__ == "__main__":
-    user_input = str(input("Enter game description!: "))
-    pred_genre = pred_genre(user_input)
-    print(f"Predicted Genre: {pred_genre}")
-
+    user_input = input("Enter game description!: ")
+    predicted_genre = pred_genre(user_input)  
+    print(f"Predicted Genre: {predicted_genre}")
